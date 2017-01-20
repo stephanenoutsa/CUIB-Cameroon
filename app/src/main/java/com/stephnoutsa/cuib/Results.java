1c454e4a896babc5cb74610ac4e367b77c5bde8b
Steph Noutsa <stephanenoutsa@yahoo.com>
2017-01-20 12:01:47 +0100

    Directs user to Transaction activity when Get Results button is clicked in Results activity

--- app/src/main/java/com/stephnoutsa/cuib/Results.java
+++ app/src/main/java/com/stephnoutsa/cuib/Results.java
@@ -1,15 +1,9 @@
 package com.stephnoutsa.cuib;
 
-import android.app.DownloadManager;
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.content.Intent;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,32 +12,17 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.stephnoutsa.cuib.utils.CuibService;
 import com.stephnoutsa.cuib.utils.MyDBHandler;
-import com.stephnoutsa.cuib.utils.RetrofitHandler;
-
-import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class Results extends AppCompatActivity {
 
     Context context = this;
     MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
-    DownloadManager downloadManager;
     TextView selectResults, yearLabel, semesterLabel;
     EditText yearField;
     Spinner semSpinner;
     int pos = 0;
-    String results = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,52 +74,15 @@ public class Results extends AppCompatActivity {
         String[] semesters = getResources().getStringArray(R.array.semester_options);
         final String semester = semesters[pos];
 
-        String matricule = dbHandler.getStudent().getMatricule();
-
         if (yearIsValid(yr)) {
             // Make year url-friendly
             final String year = convertYear(yr);
 
-            // Check if student is connected to a network
-            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isConnected()) {
-                /** Get corresponding results from server */
-                RetrofitHandler retrofitHandler = new RetrofitHandler();
-
-                CuibService cuibService = retrofitHandler.create();
-
-                try {
-                    Call<com.stephnoutsa.cuib.models.Results> call = cuibService.getResults(URLEncoder.encode(year, "UTF-8"), URLEncoder.encode(semester, "UTF-8"), URLEncoder.encode(matricule, "UTF-8"));
-                    call.clone().enqueue(new Callback<com.stephnoutsa.cuib.models.Results>() {
-                        @Override
-                        public void onResponse(Call<com.stephnoutsa.cuib.models.Results> call, Response<com.stephnoutsa.cuib.models.Results> response) {
-                            int statusCode = response.code();
-                            if (statusCode == 200) {
-                                com.stephnoutsa.cuib.models.Results r = response.body();
-
-                                results = r.getUrl();
-
-                                if (results != null) {
-                                    // Download the results
-                                    downloadResults(year, semester);
-                                } else {
-                                    Toast.makeText(context, getString(R.string.not_found), Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<com.stephnoutsa.cuib.models.Results> call, Throwable t) {
-                            Toast.makeText(context, getString(R.string.network_error), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Toast.makeText(context, getString(R.string.no_network), Toast.LENGTH_SHORT).show();
-            }
+            // Launch Transaction activity
+            Intent i = new Intent(this, Transaction.class);
+            i.putExtra("year", year);
+            i.putExtra("semester", semester);
+            startActivity(i);
         }
     }
 
@@ -173,42 +115,6 @@ public class Results extends AppCompatActivity {
         }
 
         return year;
-    }
-
-    // Save results to device
-    public void downloadResults(String year, String semester) {
-        if (results != null) {
-            try {
-                /** Download the results if available*/
-                downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse(results);
-
-                // Set the file name
-                /*Date date = new Date();
-                DateFormat df = new SimpleDateFormat("dd-MM-yyyy_hh-mm-ss_a", Locale.ENGLISH);
-                String time = df.format(date.getTime());*/
-                String filename = getString(R.string.rs_filename) + "_" + year + "_s" + semester + ".png" ;
-
-                // Set download settings
-                DownloadManager.Request request = new DownloadManager.Request(uri);
-                request.setTitle(getString(R.string.rs_download_title)).
-                        setDescription(getString(R.string.rs_download_desc)).
-                        setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED).
-                        //setVisibleInDownloadsUi(true).
-                        //setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI).
-                                setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, filename);
-
-                // Allow file to be scanned by MediaScanner
-                request.allowScanningByMediaScanner();
-
-                // Add download to queue
-                downloadManager.enqueue(request);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Toast.makeText(this, getString(R.string.no_results), Toast.LENGTH_SHORT).show();
-        }
     }
 
 }
