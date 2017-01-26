ced1f70a3ec07c487dfda562bbeacae4046e9d8b
Steph Noutsa <stephanenoutsa@yahoo.com>
2017-01-26 11:15:21 +0100

    Creates the Payment model

---
+++ app/src/main/java/com/stephnoutsa/cuib/models/Payment.java
@@ -0,0 +1,77 @@
+package com.stephnoutsa.cuib.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by stephnoutsa on 1/26/17.
+ */
+
+public class Payment {
+
+    // Private variables
+    @SerializedName("id")
+    int id;
+
+    @SerializedName("date")
+    String date;
+
+    @SerializedName("amount")
+    String amount;
+
+    @SerializedName("school")
+    String school;
+
+    // Empty constructor
+    public Payment() {
+
+    }
+
+    // Constructor
+    public Payment(int id, String date, String amount, String school) {
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.school = school;
+    }
+
+    // Constructor
+    public Payment(String date, String amount, String school) {
+        this.date = date;
+        this.amount = amount;
+        this.school = school;
+    }
+
+    // Getter and setter methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+}
